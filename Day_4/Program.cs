7b7a074dd40d7a6b29a7a50a336d9301768b5a18
Agnese <fodza@inbox.lv>
2018-05-09 19:21:22 +0300

    Day_4.2
    2.versija

--- Day_4/Program.cs
+++ Day_4/Program.cs
@@ -10,6 +10,39 @@ namespace Day_4
     {
         static void Main(string[] args)
         {
+            int mainigais = 3;
+
+            // < 
+            // >
+            // == lieto lai salidzinatu vertibas(2 x "=" )
+            // !=  2 vertibas nav vienadas
+            // >= lielaks vienads
+            // <= mazaks vienads
+
+            if(mainigais < 5)
+            {
+                Console.WriteLine("mazaks");
+
+            }
+            else
+            {
+                Console.WriteLine("lielaks");
+                
+
+            }
+
+            string mainigais2 = "si ir virkne";
+            if (mainigais2 == "si ir virkne")
+            {
+                Console.WriteLine("vienads");
+            }
+            else
+            {
+                Console.WriteLine("nav vienads");
+
+
+            }
+            Console.ReadLine();
         }
     }
 }
